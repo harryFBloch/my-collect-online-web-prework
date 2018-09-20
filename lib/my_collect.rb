1555d1ecab51b14b0629fa4d7864e4393472a939
Harry Bloch <harryfbloch@gmail.com>
2018-09-20 17:51:30 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -14,7 +14,8 @@ def my_collect(array)
 end
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-puts my_collect(array) do |name|
+newArray = my_collect(array) do |name|
   name.split(" ").first
 end
 
+puts newArray
