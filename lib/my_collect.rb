fe369cd8552304f13c1c8be338b2e663f2e23cab
Harry Bloch <harryfbloch@gmail.com>
2018-09-20 17:48:23 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-def my
+def my_collect(array)
+  if !array.empty?
+    newArray = []
+    i = 0 
+    while i < array.length 
+    yield(array[i])
+    newArray << array[i]
+    end
+  else 
+    #empty array
+  end
+  return newArray
+end
 
